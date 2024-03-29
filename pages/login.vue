b1f5fe2f7d0a9ab0da22563d5aa28ebc2d91b555
Maxwell Cofie <maxwell@hubtel.com>
2024-03-29 08:58:45 +0000

    worked on login screen

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="grid grid-cols-4 justify-center">
-    <div class="w-full flex justify-center h-[100px] bg-gray-300">
-      <button @click="signInWithOtp">
-        Sign In with Google
-        <UIcon name="i-mdi-aws"/>
+  <div class="grid grid-cols-3 justify-center items-center">
+    <div
+        class="space-y-5 p-5 w-full flex flex-col text-center mt-20 items-center justify-center h-[400px] bg-white rounded-lg border border-gray-300 col-start-2">
 
-      </button>
+      <img src="../public/imgs/btt-logo.svg" class="w-10 h-10 text-center"/>
+      <h2 class="text-4xl font-bold">Welcome to BTT <br/>Membership</h2>
+      <UButton color="primary" variant="outline" @click="signInWithGoogle()" class="px-4">
+        <UIcon name="logos:google-icon" class="text-2xl" dynamic/>
+        Sign In with Google
+      </UButton>
+      <UDivider label="OR"/>
+      <div class="flex flex-row space-x-2">
+        <UButton color="black" class="px-4">
+          <UIcon name="i-mdi-apple" class="text-2xl" dynamic/>
+          Continue with Apple
+        </UButton>
+        <UButton color="primary" variant="outline" class="px-4">
+          <UIcon name="logos:linkedin-icon" class="text-2xl" dynamic/>
+          Continue with LinkedIn
+        </UButton>
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +31,7 @@ const supabase = useSupabaseClient()
 const email = ref('')
 const password = ref('')
 
-const signInWithOtp = async () => {
+const signInWithGoogle = async () => {
 
   await supabase.auth.signInWithOAuth({
     provider: 'google',
